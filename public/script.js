037d833a2660f6d0a324e050a45c433ee95180be
Zabeeh Ullah Babar <zabi.babar@yahoo.com>
2019-03-20 15:09:05 -0400

    Negative score fix

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-//Sample code for get and post provided by Spenser 
+//Sample code for get and post provided by Spenser
 function get(url) {
   return new Promise((resolve, reject) => {
     const http = new XMLHttpRequest();
@@ -24,74 +24,70 @@ function post(url, data) {
   });
 }
 
-function login () {
-   //stores the username for the current session in local storage
-   var username = document.getElementById("uname").value;
-   localStorage.setItem("username", username);
-   
-   //if a username is entered the score is retrieved, and main window opened
-   if (username){
-      //using the provided function to get the score and saving it local storage
-      get("http://127.0.0.1:3000/"+username).then(function(response){
-      if(response.status ==  200){
-         var score = response.data.score;
-         localStorage.setItem("score", score);
-       }
-      else {
-         //User not found.
-         //response.data is null
-         console.log("User not found");
-         post("http://127.0.0.1:3000/"+username, { score: 0 }); //create a new user.
+//Runs when login page is loaded
+function login() {
+  //stores the username for the current session in local storage
+  var username = document.getElementById("uname").value;
+  localStorage.setItem("username", username);
+
+  //if a username is entered the score is retrieved, and main window opened
+  if (username) {
+    //using the provided function to get the score and saving it local storage
+    get("http://127.0.0.1:3000/" + username).then(function(response) {
+      if (response.status == 200) {
+        var score = response.data.score;
+        localStorage.setItem("score", score);
+      } else {
+        //User not found.
+        //response.data is null
+        console.log("User not found");
+        post("http://127.0.0.1:3000/" + username, { score: 0 }); //create a new user.
       }
-      window.open('main.html', "_self");
-      });
-   }
+      window.open("main.html", "_self");
+    });
+  }
 }
 
-//Runs when the page is loaded
+//Runs when the main page is loaded
 function loadScore() {
-   //Retrieves the username
-   var username = localStorage.getItem("username");
-   get("http://127.0.0.1:3000/"+username).then(function(response){ 
-      if(response.status == 200){
-         
-         var score = parseInt(response.data.score);
-         
-         if ((score%3 == 0) && (score%5 == 0))
-            score = "FIZZBUZZ";
-         else if (score%3 == 0)
-            score = "FIZZ";  
-         else if (score%5 == 0)
-            score = "BUZZ";
-   
-         //New score displayed
-         document.getElementById("numDisplay").innerHTML = score; //Display the score
-         document.getElementById("hiddenCounter").innerHTML = parseInt(response.data.score);//Save the hidden counter
-      }   
-      else
-         document.getElementById("numDisplay").innerHTML = "Click increment to begin!";   
-   });
+  //Retrieves the username
+  var username = localStorage.getItem("username");
+  get("http://127.0.0.1:3000/" + username).then(function(response) {
+    if (response.status == 200) {
+      var score = parseInt(response.data.score);
+
+      document.getElementById("hiddenCounter").innerHTML = score; //Save the hidden counter
+      if (score) {
+        if (score % 3 == 0 && score % 5 == 0) score = "FIZZBUZZ";
+        else if (score % 3 == 0) score = "FIZZ";
+        else if (score % 5 == 0) score = "BUZZ";
+      }
+      //New score displayed
+      document.getElementById("numDisplay").innerHTML = score; //Display the score
+    } else document.getElementById("numDisplay").innerHTML = "Click increment to begin!";
+  });
 }
 
-function increment () {
-   //Retrieves the username
-   var username = localStorage.getItem("username");
-   //Gets the score when increment was clicked
-   let currVal = parseInt(document.getElementById("hiddenCounter").innerHTML,10);  
-   let newVal = currVal +1; //new score
-   
-   //posts and set the new score 
-   post("http://127.0.0.1:3000/"+username, { score: newVal });
-   document.getElementById("hiddenCounter").innerHTML= newVal;
-   
-   //Fizz buzz calculation
-   if ((newVal%3 == 0) && (newVal%5 == 0))
-      newVal = "FIZZBUZZ";
-   else if (newVal%3 == 0)
-      newVal = "FIZZ";  
-   else if (newVal%5 == 0)
-      newVal = "BUZZ";
-   
-   //New score displayed
-   document.getElementById("numDisplay").innerHTML = newVal;
+//When increment button on the main page is clicked
+function increment() {
+  //Retrieves the username
+  var username = localStorage.getItem("username");
+  //Gets the score when increment was clicked
+  let currVal = parseInt(
+    document.getElementById("hiddenCounter").innerHTML,
+    10
+  );
+  let newVal = currVal + 1; //new score
+
+  //posts and set the new score
+  post("http://127.0.0.1:3000/" + username, { score: newVal });
+  document.getElementById("hiddenCounter").innerHTML = newVal;
+
+  //Fizz buzz calculation
+  if (newVal % 3 == 0 && newVal % 5 == 0) newVal = "FIZZBUZZ";
+  else if (newVal % 3 == 0) newVal = "FIZZ";
+  else if (newVal % 5 == 0) newVal = "BUZZ";
+
+  //New score displayed
+  document.getElementById("numDisplay").innerHTML = newVal;
 }
--- server.js
+++ server.js
@@ -1,79 +1,76 @@
-'use strict';
+"use strict";
 
-let fs = require('fs');
-const users = JSON.parse(fs.readFileSync('users.json')); //Database file
+var fs = require("fs");
+var data = fs.readFileSync("users.json");
+const users = JSON.parse(data);
 
-let express = require('express');
-let bodyParser = require('body-parser');
-let app = express();
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
 
 // Configure express settings
-app.set('trust proxy', true);
-app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
-app.use(bodyParser.json({limit: '10mb'}));
+app.set("trust proxy", true);
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(bodyParser.json({ limit: "10mb" }));
 
-//Avoid CORS error
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 // routes
-app.get('/:id', function(req, res){
-   let id = req.params.id;
-   console.log("GET request received at /" + id);
-   res.type('application/json');
-   if (users.hasOwnProperty(id)){
-      //If user exists Status code = 200
-      res.status(200).send({ id: id, score: users[id]});
-   }
-   else{
-      //If user doesn't exist Status code = 404
-      res.status(404).send({});
-   } 
+app.get("/:id", function(req, res) {
+  var id = req.params.id;
+  console.log("GET request received at /" + id);
+  res.type("application/json");
+  if (users.hasOwnProperty(id)) {
+    //If user exists Status code = 200
+    res.status(200).send({ id: id, score: users[id] });
+  } else {
+    //If user doesn't exist Status code = 404
+    res.status(404).send(null);
+  }
 });
 
-app.post('/:id', function(req, res){
-   let id = req.params.id;
-   console.log("POST request received at /" + id);
+app.post("/:id", function(req, res) {
+  var id = req.params.id;
+  console.log("POST request received at /" + id);
 
-   res.type('application/json');
-   let score = Number(req.body.score);
-   
-   //If user already exists
-   if (users.hasOwnProperty(id)){
-      if (!score || score < 0){
-         // Invalid score, Status 400, content {Error: String}
-         res.status(400).send({ Error: "Invalid score"});
+  res.type("application/json");
+  var score = Number(req.body.score);
 
-      }
-      else{ //Update the user
-         users[id] = score;
-         var data = JSON.stringify(users, null, 2);
-         fs.writeFile('users.json', data, function(err){
-            //Couldn't write, Status 500, content {Error: String}
-            if (err) res.status(500).send({ Error: err});
-
-         });
-         //User updated successfully, Status 200
-         res.status(200).send({ id: id, score: users[id]});
-      }
-   }
-   else{
-      //User Created, Status 201 
+  if (!score || score < 0) {
+    // Invalid score, Status 400, content {Error: String}
+    res.status(400).send({ Error: "Invalid score" });
+  } else {
+    //If user already exists
+    if (users.hasOwnProperty(id)) {
       users[id] = score;
-      let data = JSON.stringify(users, null, 2);
-      fs.writeFile('users.json', data, function(err){
-         //Couldn't write, Status 500, content {Error: String}
-         if (err) res.status(500).send({ Error: err});
+      var data = JSON.stringify(users, null, 2);
+      fs.writeFile("users.json", data, function(err) {
+        //Couldn't write, Status 500, content {Error: String}
+        if (err) res.status(500).send({ Error: err });
       });
-      res.status(201).send({ id: id, score: users[id]});
-   }
-   
+      //User updated successfully, Status 200
+      res.status(200).send({ id: id, score: users[id] });
+    } else {
+      //User Created, Status 201
+      users[id] = score;
+      var data = JSON.stringify(users, null, 2);
+      fs.writeFile("users.json", data, function(err) {
+        //Couldn't write, Status 500, content {Error: String}
+        if (err) res.status(500).send({ Error: err });
+      });
+      res.status(201).send({ id: id, score: users[id] });
+    }
+  }
 });
 
-const port = process.env.port||3000;
-app.listen(port, function(){
-   console.log(`Server is running on port ${port}`);
+const port = process.env.port || 3000;
+app.listen(port, function() {
+  console.log(`Server is running on port ${port}`);
 });
